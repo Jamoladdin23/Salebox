fa3167e9dfc7dae196a420bbcd6cec28dc769163
Jamoladdin <djamazar23@gmail.com>
2024-07-02 11:37:28 +0200

    change template

--- salebox/urls.py
+++ salebox/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Product_list.as_view(), name='Product_list'),
-    path('products/', views.MixBoxList.as_view(), name='box_list')
+    path('products/', views.Product_list.as_view(), name='Product_list'),
+    path('', views.MixBoxList.as_view(), name='box_list')
 
 ]
